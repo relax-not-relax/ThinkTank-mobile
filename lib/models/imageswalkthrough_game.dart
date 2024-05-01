c7898c75712705ef6b0476db23b1f42d0ce350ac
Nguyễn Bảo Long <104144643+LongIniesta@users.noreply.github.com>
2024-05-01 20:51:55 +0700

    Fix getDifficultGame

--- lib/models/imageswalkthrough_game.dart
+++ lib/models/imageswalkthrough_game.dart
@@ -52,30 +52,29 @@ class ImagesWalkthroughGame {
     return data;
   }
 
-  Future<double> getTimeImagesWalkthrough(int level) async {
-    if (level == 1) {
-      imgCount = 6;
-    }
-    if (level >= 2 && level <= 5) {
-      imgCount = 8;
-    } else if (level >= 6 && level <= 10) {
-      imgCount = 12;
-    } else if (level >= 11 && level <= 20) {
-      imgCount = 16;
-    } else if (level >= 21 && level <= 30) {
-      imgCount = 20;
-    } else if (level >= 31 && level <= 40) {
-      imgCount = 24;
-    } else {
-      imgCount = 28;
-    }
+  Future<double> getDifficultyImagesWalkthrough(int level) async {
+    // if (level == 1) {
+    //   imgCount = 6;
+    // } else if (level >= 2 && level <= 5) {
+    //   imgCount = 8;
+    // } else if (level >= 6 && level <= 10) {
+    //   imgCount = 12;
+    // } else if (level >= 11 && level <= 20) {
+    //   imgCount = 16;
+    // } else if (level >= 21 && level <= 30) {
+    //   imgCount = 20;
+    // } else if (level >= 31 && level <= 40) {
+    //   imgCount = 24;
+    // } else {
+    //   imgCount = 28;
+    // }
 
     int c = (level % 10 <= 1) ? level ~/ 10 : ((level ~/ 10) + 1);
-    if (level == 1)
-      return (3.15 * imgCount);
-    else {
-      return (await getTimeImagesWalkthrough(level - 1) -
-          (3.15 - 0.48) / pow(2, c) / 10 * imgCount);
+    if (level == 1) {
+      return (3.15);
+    } else {
+      return (await getDifficultyImagesWalkthrough(level - 1) -
+          (3.15 - 0.48) / pow(2, c) / 10);
     }
   }
 
@@ -108,7 +107,7 @@ class ImagesWalkthroughGame {
           await SharedPreferencesHelper.getImageResourceByTopicId(40);
       listTmp.shuffle();
 
-      time = await getTimeImagesWalkthrough(level);
+      print('time: $time');
 
       if (level == 1) {
         imgCount = 6;
@@ -131,6 +130,7 @@ class ImagesWalkthroughGame {
 
         gameData = getImageData(imgCount, listTmp);
       }
+      time = await getDifficultyImagesWalkthrough(level) * (imgCount - 1);
     }
   }
 }
